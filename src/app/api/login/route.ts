4b40c85032d5aaa99b6ff9fb5f685a83d036d5a9
codemmk <2107228359@qq.com>
2025-01-20 10:56:50 +0800

    Complate: login function

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,15 +1,49 @@
 import { useServerTool } from "@/utils/useServerTool";
 import { NextRequest } from "next/server";
+import bcrypt from 'bcryptjs'
+import prisma from "@/utils/usePrisma";
+const { compare } = bcrypt
+import jwt from 'jsonwebtoken'
+import { cookies } from "next/headers";
 
 export async function POST(request: NextRequest) {
   try {
     useServerTool.setRequest(request)
 
     const body = await useServerTool.getBody()
-  
+
+    const user = await prisma.user.findFirst({
+      where: {
+        username: body.username
+      }
+    })
+
+    if(!user) {
+      return useServerTool.responseError({
+        message: "用户名不存在"
+      })
+    }
+
+    const isTheSame = await compare(body.password, user.password)
+    if(!isTheSame) {
+      return useServerTool.responseError({
+        message: "密码错误"
+      })
+    }
+
+    const token = jwt.sign({
+      id: user.id,
+      username: user.username
+    }, process.env.JWT_SECRET as string, {
+      expiresIn: '24h'
+    })
+
+    const cookieStore = await cookies()
+    cookieStore.set('token', token)
+
     return useServerTool.responseSuccess({
       data: {
-        body
+        token
       }
     })
   } catch (error) {
