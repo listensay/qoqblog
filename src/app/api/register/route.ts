52e4fb56ded733d92c0b9d1f97b7c622497eb0fb
codemmk <2107228359@qq.com>
2025-01-20 10:38:15 +0800

    Complete: registartion function

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,34 +1,64 @@
 import prisma from "@/utils/usePrisma";
 import { useServerTool } from "@/utils/useServerTool";
 import { NextRequest } from "next/server";
+import bcrypt from 'bcryptjs'
+const { hashSync } = bcrypt
 
 export async function POST(request: NextRequest) {
   try {
     useServerTool.setRequest(request)
 
     const body = await useServerTool.getBody()
-    
-    const user = await prisma.user.create({
-      data: {
-        username: body.username,
-        email: body.email,
-        password: body.password
+
+    // 检测用户名
+    const user = await prisma.user.findFirst({
+      where: {
+        username: body.username
       }
     })
-  
-    if (!user) {
+
+    if(user) {
+      return useServerTool.responseError({
+        message: "用户名已存在"
+      })
+    }
+
+    // 检测邮箱
+    const email = await prisma.user.findFirst({
+      where: {
+        email: body.email
+      }
+    })
+
+    if(email) {
+      return useServerTool.responseError({
+        message: "邮箱已存在"
+      })
+    }
+
+    const saltRounds = 10
+    const password = await hashSync(body.password, saltRounds)
+
+    // 开始注册
+    try {
+      await prisma.user.create({
+        data: {
+          username: body.username,
+          email: body.email,
+          password: password
+        }
+      })
+      return useServerTool.responseSuccess({
+        message: "注册成功"
+      })
+    } catch (error) {
       return useServerTool.responseError({
         message: "注册失败"
       })
     }
-  
-    return useServerTool.responseSuccess({
-      message: "注册成功"
-    })
   } catch (error) {
-    console.log(error)
     return useServerTool.responseError({
-      message: "注册失败",
+      message: "系统错误",
       status: 400
     })
   }
