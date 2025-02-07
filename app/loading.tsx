8f20736df9f28221c0f1e42d2140a9716e9185cb
codemmk <2107228359@qq.com>
2025-02-07 15:33:42 +0800

    添加页面加载loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,8 +1,11 @@
+import { Loader } from '@mantine/core'
 import React, { memo } from 'react'
 
 const loading = memo(() => {
   return (
-    <div>loading</div>
+    <div className='flex justify-center items-center h-screen bg-white'>
+      <Loader color="rgba(255, 130, 130, 1)" />
+    </div>
   )
 })
 
