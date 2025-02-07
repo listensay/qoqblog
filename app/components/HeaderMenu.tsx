2d0a93a6a3ffc65a6894e067280181b781695ed2
codemmk <2107228359@qq.com>
2025-02-07 15:34:51 +0800

    导航高亮

--- app/components/HeaderMenu.tsx
+++ app/components/HeaderMenu.tsx
@@ -1,39 +1,66 @@
-import React, { memo } from 'react'
-import Link from 'next/link'
-import { IconBrandBilibili, IconBrandGithub, IconBrandXFilled } from '@tabler/icons-react'
+"use client";
+
+import React, { memo } from 'react';
+import Link from 'next/link';
+import { IconBrandBilibili, IconBrandGithub, IconBrandXFilled, IconHome, IconUserCircle } from '@tabler/icons-react';
+import { usePathname } from 'next/navigation';
 
 const menu = memo(() => {
-  return (
-    <div className='w-full'>
-        <div className="flex justify-between w-full px-6">
-          <nav>
-            <ul className="flex gap-4">
-              <li><Link href="/">Home</Link></li>
-              <li><Link href="/about">About</Link></li>
-            </ul>
-          </nav>
-          <div>
-            <ul className='flex gap-2'>
-              <li>
-                <Link href={`https://space.bilibili.com/344928233`} target="_blank">
-                  <IconBrandBilibili color='pink' />
-                </Link>
-              </li>
-              <li>
-                <Link href={`https://github.com/listensay`}>
-                  <IconBrandGithub color='black' />
-                </Link>
-              </li>
-              <li>
-                <Link href={`https://x.com/yimeng2002`}>
-                  <IconBrandXFilled />
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-    </div>
-  )
-})
+  const menus = [
+    {
+      name: '首页',
+      icon: <IconHome color="pink" />,
+      path: '/',
+    },
+    {
+      name: '关于我',
+      icon: <IconUserCircle color="#a78bfa" />,
+      path: '/about',
+    },
+  ];
 
-export default menu
+  const currentNav = usePathname();
+
+  return (
+    <div className="w-full">
+      <div className="flex justify-between w-full px-6">
+        <nav>
+          <ul className="flex gap-4">
+            {menus.map((item) => {
+              return (
+                <Link 
+                  href={item.path}
+                  className={`flex ${ item.path === currentNav ? 'text-sky-500' : '' }`}
+                  key={item.path}
+                >
+                  {item.icon} <span className="ml-2">{item.name}</span>
+                </Link>
+              );
+            })}
+          </ul>
+        </nav>
+        <div>
+          <ul className="flex gap-2">
+            <li>
+              <Link href={`https://space.bilibili.com/344928233`} target="_blank">
+                <IconBrandBilibili color="pink" />
+              </Link>
+            </li>
+            <li>
+              <Link href={`https://github.com/listensay`}>
+                <IconBrandGithub color="black" />
+              </Link>
+            </li>
+            <li>
+              <Link href={`https://x.com/yimeng2002`}>
+                <IconBrandXFilled />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+});
+
+export default menu;
