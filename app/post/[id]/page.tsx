2bfe713ee6739eedea50867cff3fd37f3ba5255a
codemmk <2107228359@qq.com>
2025-02-07 15:33:19 +0800

    时间格式化

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,13 +1,12 @@
-import Header from '@/components/Header'
 import HeaderLayout from '@/components/HeaderLayout'
 import { fetchGetPost } from '@/service/posts'
 import React, { memo } from 'react'
+import dayjs from 'dayjs'
 
 const page = memo(async ({ params }: { params: Promise<{ id: string }>}) => {
   const id = (await params).id
   const result = await fetchGetPost(Number(id))
   const post = result?.data.post
-  console.log(post)
 
   return (
     <div>
@@ -15,7 +14,7 @@ const page = memo(async ({ params }: { params: Promise<{ id: string }>}) => {
         <div className='bg-white p-8 rounded-lg shadow-md'>
           <div className='prose max-w-full mx-auto mb-8'>
             <h1>{ post.title }</h1>
-            <div>发布时间 { post.createdAt } · 阅读 { post.views }</div>
+            <div>阅读 {post.views} · 发布时间 { dayjs( post.createdAt).format('YYYY年MM月DD日 hh:ss') }</div>
           </div>
           <div className="prose max-w-full mx-auto" dangerouslySetInnerHTML={{ __html: post.content }}></div>
         </div>
