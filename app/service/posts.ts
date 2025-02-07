8e8f7cacae9f8940b5ec2af61747cff42f90e37c
codemmk <2107228359@qq.com>
2025-02-07 15:32:53 +0800

    代码注释

--- app/service/posts.ts
+++ app/service/posts.ts
@@ -1,5 +1,6 @@
 import myRequest from "~@/utils/myRequest"
 
+// 获取文章列表
 export const fetchGetPosts = async (page: number = 1, pageSize: number = 10) => {
   try {
     const result = myRequest.get('/v1/posts', {
@@ -12,6 +13,7 @@ export const fetchGetPosts = async (page: number = 1, pageSize: number = 10) => 
   }
 }
 
+// 获取文章内容
 export const fetchGetPost = async (id: number) => {
   try {
     const result = myRequest.get(`/v1/posts/${id}`)
