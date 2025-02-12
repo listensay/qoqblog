b35bd281c784b0fc235d87bed60be17ae6e20158
codemmk <2107228359@qq.com>
2025-02-12 16:03:32 +0800

    关于页面内容

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,20 @@ const page = memo(() => {
   return (
     <div>
       <HeaderLayout>
-        page
+        <div className='prose max-sm:prose-sm'>
+          <h1>🧑‍💻 关于我</h1>
+          <ul>
+            <li>我是，背影如正面。</li>
+            <li>我来自广西，是一个零零后，是一个大专人，是一个前端开发工程师。</li>
+            <li>我的兴趣爱好十分广泛，喜欢看小说、写代码、画画。</li>
+            <li>INFP-T</li>
+          </ul>
+          <h2>现状</h2>
+          <ul>
+            <li>Node全栈开发者：Vue.js，React.js，Nuxtjs，Nextjs，Prisma，Mysql</li>
+            <li>卑微寻求工作中...</li>
+          </ul>
+        </div>
       </HeaderLayout>
     </div>
   )
--- app/components/HeaderMenu.tsx
+++ app/components/HeaderMenu.tsx
@@ -9,17 +9,17 @@ const menu = memo(() => {
   const menus = [
     {
       name: '首页',
-      icon: <IconHome color="pink" />,
+      icon: <IconHome color="pink" size={18} />,
       path: '/',
     },
     {
-      name: '关于我',
-      icon: <IconBallBowling color="#00b5ad" />,
+      name: '关于',
+      icon: <IconBallBowling color="#00b5ad" size={18} />,
       path: '/about',
     },
     {
       name: '登录',
-      icon: <IconDashboard color='#f59e0b' />,
+      icon: <IconDashboard color='#f59e0b' size={18} />,
       path: '/login',
     }
   ];
@@ -27,17 +27,18 @@ const menu = memo(() => {
   const currentNav = usePathname();
 
   return (
-    <div className="w-full">
-      <div className="flex justify-between w-full px-6 max-sm:px-4 max-sm:text-sm">
+    <div className="w-full h-full">
+      <div className="flex justify-between w-full h-full px-6 max-md:px-4 max-sm:text-sm">
         <nav>
-          <ul className="flex gap-4">
+          <ul className="flex h-full">
             {menus.map((item) => {
               return (
                 <Link 
                   href={item.path}
-                  className={`flex items-center ${ item.path === currentNav ? 'text-sky-500' : '' }`}
+                  className={`flex items-center h-full text-sm relative px-4 max-md:px-2 ${ item.path === currentNav ? 'text-pink-300' : '' }`}
                   key={item.path}
                 >
+                  <div className={`${ item.path === currentNav ? 'absolute left-0 bottom-0 h-[4px] w-full bg-pink-300' : '' }`}></div>
                   {item.icon} <span className="ml-2">{item.name}</span>
                 </Link>
               );
@@ -45,7 +46,7 @@ const menu = memo(() => {
           </ul>
         </nav>
         <div>
-          <ul className="flex gap-2">
+          <ul className="flex items-center h-full gap-2">
             <li>
               <Link href={`https://space.bilibili.com/344928233`} target="_blank">
                 <IconBrandBilibili color="pink" />
